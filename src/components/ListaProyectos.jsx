7a859a77979a51fc42161dc04a4fdee3c8412451
Claudio Aguirre <manurojas147@gmail.com>
2026-05-08 16:04:08 -0300

    correccion del archivo listaproyectos y armado del css del mismo.

--- src/components/ListaProyectos.jsx
+++ src/components/ListaProyectos.jsx
@@ -1,17 +1,53 @@
-import proyectoService from "../services/proyectoServices"
 import { useState } from "react";
+import proyectoService from "../services/proyectoService";
+import "../css/listaProyectos.css";
 const ListaProyectos =()=>{
 
-    const [proyectos, setProyectos] = useState(proyectoService.obtenerProyectos());
+    const [proyectos, setProyectos] = useState(
+        proyectoService.obtenerProyectos());
+    
+    const [busqueda, setBusqueda] = useState("");
+
+    const [titulo, setTitulo] = useState("");
+    const [categoria, setCategoria] = useState("");
+    const [estado, setEstado] = useState("");
+    
     const eliminar = (id) => {
         proyectoService.eliminarProyecto(id);
         setProyectos(proyectoService.obtenerProyectos());
-    }
-    
+    };
+    const buscar = (texto) => {
+        setBusqueda(texto);
+        if (texto === "") {
+           setProyectos(proyectoService.obtenerProyectos());
+        } else {
+            setProyectos(proyectoService.buscarProyecto(texto));
+        }
+    };
+    const agregar = () => {
+        if (titulo.trim() === "" || categoria.trim() === "" || estado.trim() === ""){alert("Completa todos los campos");
+        return;}
+        const nuevoProyecto = {
+            id: Date.now(), titulo, categoria, estado};
+        proyectoService.agregarProyecto(nuevoProyecto);
+        setProyectos(proyectoService.obtenerProyectos());
+        setTitulo("");
+        setCategoria("");
+        setEstado("");
+    };
+
     return(
         <div>
-            <h2>Gestion de Proyecto Educativos</h2>
-            
+            <h2 className="titilo">Gestion de Proyecto Educativos</h2>
+
+            <div className="formulario">
+               <input type="text" placeholder="Título" value={titulo} onChange={(e) => setTitulo(e.target.value)}/>
+               <input type="text" placeholder="Categoría" value={categoria} onChange={(e) => setCategoria(e.target.value)}/>
+               <input type="text" placeholder="Estado" value={estado} onChange={(e) => setEstado(e.target.value)}/>
+               <button onClick={agregar}>Agregar Proyecto</button>
+            </div>
+            <input type="text" placeholder="Buscar proyecto..." value={busqueda} onChange={(e) => buscar(e.target.value)}/>
+
             <section>
                 <div>
                     {proyectos.map(p =>(
