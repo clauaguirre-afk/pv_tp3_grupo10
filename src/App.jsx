1c5cb9e7dd0d2e0f1245f91e1af8d9feecd9d61d
flor cordoba <florencicordoba820@gmail.com>
2026-06-01 22:49:25 -0300

    modifiqueapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import ListaProyectos from './components/ListaProyectos';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Dashboard from './components/Dashboard';
+import PerfilUsuario from './components/PerfilUsuario';
+import DetalleProyecto from './components/DetalleProyecto';
+import { Routes, Route } from 'react-router-dom';
 const App =()=>{
   return(
     <div className="contenedor2">
       <Header/>
 
       <main>
-        <ListaProyectos/>
+        <Routes>
+          <Route path='/' element={<Dashboard/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/proyectos' element={<ListaProyectos/>}/>
+          <Route path='/proyecto/:id' element={<DetalleProyecto/>}/>
+          <Route path='/perfil' element={<PerfilUsuario/>}/>
+        </Routes>
       </main>
 
       <Footer/>
