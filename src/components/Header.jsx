9852c70ed921dcbe71ef2324368ca3a95dafcbb6
dani26-byte <37930173@fi.unju.edu.ar>
2026-06-13 11:14:59 -0300

    modifique el header y agregue algunos estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
+import React, { useContext } from 'react';
 import Nav from "./Nav";
-const Header = () =>{
+import { UsuarioContext } from '../context/UsuarioContext';
+import '../css/styles.css';
+
+const Header = () => {
+    const { usuario } = useContext(UsuarioContext);
+
     return (
-        <header >
+        <header className="header-principal">
             <h1 id="titulo">Proyectos</h1>
+            
             <Nav />
+
+            {/* Tarjeta de usuario en el encabezado */}
+            <div className="header-user-card">
+                <strong className="user-name">{usuario.nombre}</strong>
+                <span className="user-rol">{usuario.rol}</span>
+            </div>
         </header>
     );
 };
+
 export default Header;
