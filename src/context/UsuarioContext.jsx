a3e3406c0cc91a1f4e0e6f436b9e2e2f790c40a1
samuelxd14 <samuel.ruiz1607@gmail.com>
2026-06-15 15:40:24 -0300

    Agrego persistencia del estado global

--- src/context/UsuarioContext.jsx
+++ src/context/UsuarioContext.jsx
@@ -1,13 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 export const UsuarioContext = createContext();
 export const UsuarioProvider = ({ children }) => {
 
-    const [usuario, setUsuario] = useState({
-        nombre: "Claudio Aguirre",
-        dni: "12345678",
-        rol: "Alumno",
-        institucion: "Facultad de Ingeniería"
+    const [usuario, setUsuario] = useState(() => {
+        const usuarioGuardado = localStorage.getItem("usuarioGlobal");
+        if (usuarioGuardado) {
+            return JSON.parse(usuarioGuardado)
+        }
+        return {
+            nombre: "Claudio Aguirre",
+            dni: "12345678",
+            rol: "Alumno",
+            institucion: "Facultad de Ingeniería"
+        };
     });
+
+    useEffect(() => {
+        localStorage.setItem("usuarioGlobal". JSON.stringify(usuario));
+    }, [usuario]);
 
     const actualizarPerfil = (nuevosDatos) => {
         setUsuario({
@@ -17,6 +27,8 @@ export const UsuarioProvider = ({ children }) => {
     };
 
     return (
-        <UsuarioContext.Provider value={{ usuario, actualizarPerfil}}>{children}</UsuarioContext.Provider>
+        <UsuarioContext.Provider value={{ usuario, actualizarPerfil}}>
+            {children}
+        </UsuarioContext.Provider>
     );
 };
