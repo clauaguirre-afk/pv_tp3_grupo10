0e799dd8dc477f1f5c7f8919250ade359d81e8c7
Martin Argota <martin.argota.sp@gmail.com>
2026-06-15 10:20:10 -0300

    Agrego UsuarioProvider en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import ListaProyectos from './components/ListaProyectos';
 import Footer from './components/Footer';
 import Header from './components/Header';
-const App =()=>{
-  return(
-    <div className="contenedor2">
-      <Header/>
+import { UsuarioProvider } from './context/UsuarioContext';
 
-      <main>
-        <ListaProyectos/>
-      </main>
+const App = () => {
+  return (
+    <UsuarioProvider>
+      <div className="contenedor2">
+        <Header/>
 
-      <Footer/>
-    </div>
- );
+        <main>
+          <ListaProyectos/>
+        </main>
+
+        <Footer/>
+      </div>
+    </UsuarioProvider>
+  );
 }
+
 export default App;
